a5ae9c874a3217fc187308c8d2f4bc334edf8c64
EscadaRebelo <rebelo1919@hotmail.com>
2025-02-18 15:43:55 +0000

    Part3 (not complete) - Scroll To Element Using JavaScriptExecutor - Working With Radio Buttons - Working With Checkboxes - Working With Tables

---
+++ src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
@@ -0,0 +1,53 @@
+package com.demoqa.pages.forms;
+
+import org.openqa.selenium.By;
+
+import static utilities.JavaScriptUtility.clickJS;
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
+public class PracticeFormPage extends FormsPage {
+
+    private By femaleRadioButton = By.id("gender-radio-2");
+    private By sportsHobbyCheckbox = By.id("hobbies-checkbox-1");
+    private By readingHobbyCheckbox = By.id("hobbies-checkbox-2");
+    private By musicHobbyCheckbox = By.id("hobbies-checkbox-3");
+
+    public void clickFemaleRadioButton() {
+        scrollToElementJS(femaleRadioButton);
+        clickJS(femaleRadioButton);
+    }
+
+    public boolean isFemaleSelected() {
+        return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSportsCheckbox() {
+        if (!find(sportsHobbyCheckbox).isSelected()) { //if not found
+            scrollToElementJS(sportsHobbyCheckbox); //scroll to element
+            clickJS(sportsHobbyCheckbox); //then click it
+        }
+    }
+    public void clickReadingCheckbox() {
+        if (!find(readingHobbyCheckbox).isSelected()) { //if not found
+            scrollToElementJS(readingHobbyCheckbox); //scroll to element
+            clickJS(readingHobbyCheckbox); //then click it
+        }
+    }
+
+    public void clickMusicCheckbox() {
+        if (!find(musicHobbyCheckbox).isSelected()) { //if not found
+            scrollToElementJS(musicHobbyCheckbox); //scroll to element
+            clickJS(musicHobbyCheckbox); //then click it
+        }
+    }
+    public void unclickReadingCheckbox() {
+        if (find(readingHobbyCheckbox).isSelected()) { //if not found
+            scrollToElementJS(readingHobbyCheckbox); //scroll to element
+            clickJS(readingHobbyCheckbox); //then click it
+        }
+    }
+
+    public boolean isReadingSelected() {
+        return find(readingHobbyCheckbox).isSelected();
+    }
+}
